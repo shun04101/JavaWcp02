8bdeed4c006cc6a5d6e68204c9940d2a3736ff3f
shun04101 <nakashimashuntarou@gmail.com>
2022-03-05 21:44:47 +0900

    [Add]3章/4章カリキュラム・確認問題

---
+++ src/System/out/Chapter04.java
@@ -0,0 +1,15 @@
+package System.out;
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "中島俊太郎";
+		System.out.println(str1);
+
+		str1 = "粉骨砕身";
+		System.out.println(str1);
+
+		final int LUC_NUMBER = 7;
+		System.out.println(LUC_NUMBER);
+	}
+}
