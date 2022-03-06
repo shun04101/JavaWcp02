e4c0aad2cc5eac69c994a37bd8ea8b6a19c5d8c0
shun04101 <nakashimashuntarou@gmail.com>
2022-03-06 21:40:56 +0900

    [Add]7章カリキュラム/確認問題

---
+++ src/Chapter07.java
@@ -0,0 +1,17 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+
+		int km = 10;
+
+		if (km <= 5) {
+			System.out.println("とても近いです");
+		} else if (km <= 10) {
+			System.out.println("近いです");
+		} else if (km <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+	}
+}
